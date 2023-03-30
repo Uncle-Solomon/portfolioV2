e2038307c5971ab583118ee4f2506e54bb67ef50
Ameh Solomon Onyeke <amehsolomon46@gmail.com>
2023-03-30 16:28:58 +0100

    Update anchor tags

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/tw-elements/dist/js/**/*.js",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
-    safelist: [
-      "animate-[fade-in_1s_ease-in-out]",
-      "animate-[fade-in-down_1s_ease-in-out]",
-    ],
   },
   plugins: [],
 };
