93ea665b497162b1c92475118edce65a3e286dc1
Ameh Solomon Onyeke <amehsolomon46@gmail.com>
2023-04-13 13:31:50 +0100

    Made adjustments

--- src/App.jsx
+++ src/App.jsx
@@ -14,17 +14,19 @@ function App() {
     setDarkMode((prevDarkMode) => !prevDarkMode);
   };
   return (
-    <div
-      className={`h-full w-full mx-auto py-2 
-    ${darkMode ? "dark" : ""} dark:bg-gray-900`}
-    >
-      <main className="bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-900">
-        <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
-        <Hero />
-        <About />
-        <Portfolio />
-        <Footer />
-      </main>
+    <div className={darkMode ? "dark" : ""}>
+      <div
+        className="h-full w-full mx-auto py-2 
+     dark:bg-gray-900"
+      >
+        <main className="bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-900">
+          <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+          <Hero />
+          <About />
+          <Portfolio />
+          <Footer />
+        </main>
+      </div>
     </div>
   );
 }
